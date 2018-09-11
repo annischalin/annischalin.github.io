0acc8b1c4ff8fb818f65bfb82005528f1035e805
annischalin <anni.schalin@gmail.com>
2018-09-11 10:31:13 +0300

    hotjar
    hotjar

--- guessthenumber.js
+++ guessthenumber.js
@@ -1,15 +1,29 @@
+function getRandomInteger() {
+	return Math.floor(Math.random()*10) + 1
+}
 
-var random = Math.floor(Math.random()*10) + 1;
+function compareNumbers(first, second) {
+	if (first === second) {
+		return true
+	}
+	else {
+		return false
+	}
+}
+
+var random = getRandomInteger();
 
 function guessTheNumber() {
+	console.log(random);
     var guess = Number(document.getElementById("number").value);
-    
-    if (guess === random) {
-        alert("You guessed it!");
-    } else if ( guess < 0 || guess%1 !== 0 || guess > 10)  {
+     if ( guess < 0 || guess%1 !== 0 || guess > 10)  {
         alert("The number has to be a whole number between 0 and 10.")
-    } else {
+    } else if (compareNumbers (guess, random)===false) {
         alert("Wrong, try again!");
     }
+	else if(compareNumbers (guess, random)===true) {
+		alert("You guessed it!");
+		random = getRandomInteger();
+	}
 }
 
